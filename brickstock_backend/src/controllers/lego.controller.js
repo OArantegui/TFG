3832af0b295a13191b10e0d2937152ff80d131b3
Oscar <oscar.arantegui@gmail.com>
2026-01-27 12:01:06 +0100

    Fixed images, adding them into node directly (controller and routes)

--- brickstock_backend/src/controllers/lego.controller.js
+++ brickstock_backend/src/controllers/lego.controller.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const rebrickableService = require('../services/rebrickable.service');
 
 const getThemes = async (req, res) => {
@@ -22,8 +23,33 @@ const getSets = async (req, res) => {
         res.status(500).json({ message: 'Error al obtener sets', error: error.message });
     }
 };
+const getImageProxy = async (req, res) => {
+    const { url } = req.query; // Recibimos la URL real como parámetro
+    
+    if (!url) {
+        return res.status(400).send('Falta el parámetro url');
+    }
 
+    try {
+        // El backend pide la imagen a Rebrickable (sin CORS de por medio)
+        const response = await axios({
+            url: url,
+            method: 'GET',
+            responseType: 'stream' // Importante: la bajamos como flujo de datos
+        });
+
+        // Copiamos las cabeceras de tipo de imagen (jpg/png)
+        res.setHeader('Content-Type', response.headers['content-type']);
+        
+        // "Enchufamos" la descarga directamente a la respuesta del cliente
+        response.data.pipe(res);
+    } catch (error) {
+        console.error("Error proxy imagen:", error.message);
+        res.status(404).send('Imagen no encontrada');
+    }
+};
 module.exports = {
     getThemes,
-    getSets
+    getSets,
+    getImageProxy
 };
--- brickstock_backend/src/routes/lego.routes.js
+++ brickstock_backend/src/routes/lego.routes.js
@@ -5,5 +5,6 @@ const legoController = require('../controllers/lego.controller');
 // Definimos las rutas: http://localhost:3000/api/lego/...
 router.get('/themes', legoController.getThemes);
 router.get('/sets/:themeId', legoController.getSets);
+router.get('/image-proxy', legoController.getImageProxy);
 
 module.exports = router;

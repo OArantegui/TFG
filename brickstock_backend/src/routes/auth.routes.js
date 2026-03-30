43fb74ff75e597f3703b1e87f5f8d9f6522b95ec
Oscar Arantegui <oscar.arantegui@gmail.com>
2026-03-30 16:10:12 +0200

    fix missed import auth

--- brickstock_backend/src/routes/auth.routes.js
+++ brickstock_backend/src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login } = require('../controllers/auth.controller');
+const { register, login, updateUser } = require('../controllers/auth.controller');
+
+const authMiddleware = require('../middlewares/auth.middleware');
 
 // Ruta para registrar un usuario (POST http://localhost:3000/api/auth/register)
 router.post('/register', register);
 router.post('/login', login);
+
 router.put('/profile', authMiddleware, updateUser);
 
 module.exports = router;

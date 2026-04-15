414059fef05e26c9a6df545dcbfa2d47ff4987ba
OArantegui <OArantegui@users.noreply.github.com>
2026-04-15 14:16:30 +0000

    deploy: 43dc759081acdfb049f71519b863dff2349855ba

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1901125139" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2009601103" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
